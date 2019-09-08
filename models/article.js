62487756e462cf3a77cf553596d3d0ff0223f40b
Harrison, Rachel <harrison.e.rachel@gmail.com>
2019-09-08 12:30:38 -0500

    removed saved criteria from article schema

--- models/article.js
+++ models/article.js
@@ -24,7 +24,7 @@ var ArticleSchema = new Schema({
     note: {
         type: Schema.Types.ObjectId,
         ref: "Note"
-    }
+    },
 });
 
 // create model from schema above using mongoose's model method
